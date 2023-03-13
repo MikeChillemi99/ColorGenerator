1a2363bf391c4fb8a3747f0a3765cd854c563e01
Mike Chillemi <91112647+MikeChillemi99@users.noreply.github.com>
2023-03-13 21:02:41 +0100

    Update 13.03.23
    Add lock and lock-open to chose color to keep, end of the project

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,48 @@ let colorTableName = [colorName1, colorName2, colorName3]
 let copyColorZone1 = document.getElementById('colorShow1');
 let copyColorZone2 = document.getElementById('colorShow2');
 let copyColorZone3 = document.getElementById('colorShow3');
+let lock1 = document.getElementById('lock1');
+let lock2 = document.getElementById('lock2');
+let lock3 = document.getElementById('lock3');
+
+lock1.addEventListener('click', function(event) {
+  if (lock1.classList.contains('fa-lock-open'))
+  {
+    lock1.classList.remove("fa-lock-open");
+    lock1.classList.add("fa-lock");
+  }
+  else
+  {
+    lock1.classList.remove("fa-lock");
+    lock1.classList.add("fa-lock-open");
+  }
+});
+
+lock2.addEventListener('click', function(event) {
+  if (lock2.classList.contains('fa-lock-open'))
+  {
+    lock2.classList.remove("fa-lock-open");
+    lock2.classList.add("fa-lock");
+  }
+  else
+  {
+    lock2.classList.remove("fa-lock");
+    lock2.classList.add("fa-lock-open");
+  }
+});
+
+lock3.addEventListener('click', function(event) {
+  if (lock3.classList.contains('fa-lock-open'))
+  {
+    lock3.classList.remove("fa-lock-open");
+    lock3.classList.add("fa-lock");
+  }
+  else
+  {
+    lock3.classList.remove("fa-lock");
+    lock3.classList.add("fa-lock-open");
+  }
+});
 
 
 const generateColor = () => {
@@ -12,10 +54,56 @@ const generateColor = () => {
   }
 
 const newColor = () => {
-  for (let i = 0; i < 3; i++)
+
+  if (lock1.classList.contains('fa-lock') && lock2.classList.contains('fa-lock-open') && lock3.classList.contains('fa-lock-open'))
   {
+    colorTable[1].style.backgroundColor = "#" + generateColor();
+    colorTableName[1].value = "#" + generateColor();
+    colorTable[2].style.backgroundColor = "#" + generateColor();
+    colorTableName[2].value = "#" + generateColor();
+  }
+  else if (lock2.classList.contains('fa-lock') && lock1.classList.contains('fa-lock-open') && lock3.classList.contains('fa-lock-open'))
+  {
+    colorTable[0].style.backgroundColor = "#" + generateColor();
+    colorTableName[0].value = "#" + generateColor();
+    colorTable[2].style.backgroundColor = "#" + generateColor();
+    colorTableName[2].value = "#" + generateColor();
+  }
+  else if (lock3.classList.contains('fa-lock') && lock1.classList.contains('fa-lock-open') && lock2.classList.contains('fa-lock-open'))
+  {
+    colorTable[0].style.backgroundColor = "#" + generateColor();
+    colorTableName[0].value = "#" + generateColor();
+    colorTable[1].style.backgroundColor = "#" + generateColor();
+    colorTableName[1].value = "#" + generateColor();
+  }
+  else if (lock1.classList.contains('fa-lock') && lock2.classList.contains('fa-lock') && lock3.classList.contains('fa-lock-open'))
+  {
+    colorTable[2].style.backgroundColor = "#" + generateColor();
+    colorTableName[2].value = "#" + generateColor();
+
+  }
+  else if (lock1.classList.contains('fa-lock') && lock3.classList.contains('fa-lock') && lock2.classList.contains('fa-lock-open'))
+  {
+    colorTable[1].style.backgroundColor = "#" + generateColor();
+    colorTableName[1].value = "#" + generateColor();
+
+  }
+  else if (lock2.classList.contains('fa-lock') && lock3.classList.contains('fa-lock') && lock1.classList.contains('fa-lock-open'))
+  {
+    colorTable[0].style.backgroundColor = "#" + generateColor();
+    colorTableName[0].value = "#" + generateColor();
+  }
+  else if (lock1.classList.contains('fa-lock') && lock2.classList.contains('fa-lock') && lock3.classList.contains('fa-lock'))
+  {
+
+  }
+  else
+  {
+    for (i = 0; i < 3; i++)
+    {
       colorTable[i].style.backgroundColor = "#" + generateColor();
       colorTableName[i].value = "#" + generateColor();
+    }
   }
 }
 
